2e34450a3b4b412ec4706442be961bdc74fbb8b1
Terence <terence@madhead.com>
2020-11-20 16:03:28 +0800

    remove async in createConnection

--- src/createConnection.ts
+++ src/createConnection.ts
@@ -2,7 +2,7 @@ import * as Datastore from "@google-cloud/datastore";
 import {Connection} from "./Connection";
 import {IConnectionOptions} from "./types";
 
-export async function createConnection(options: IConnectionOptions) {
+export function createConnection(options: IConnectionOptions) {
     let datastore: Datastore.Datastore;
     const {keyFilename, clientEmail, privateKey} = options as any;
 
